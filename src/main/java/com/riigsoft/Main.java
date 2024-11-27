f5d59a3e1da3a20a6ede90f07905c81f013a1f13
Abdallah Mahmoud <javabyabdallah@gmail.com>
2024-11-27 09:00:11 +0300

    Initial commit.

---
+++ src/main/java/com/riigsoft/Main.java
@@ -0,0 +1,10 @@
+package com.riigsoft;
+
+public class Main {
+    public static void main(String[] args) {
+//       DataTypes dt = new DataTypes();
+//        dt.dataTypes();
+        Operators op = new Operators();
+        op.operators();
+    }
+}
