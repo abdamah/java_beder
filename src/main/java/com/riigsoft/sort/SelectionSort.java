6ac0e1bf21d8a14cfc61bad81b791e597bb83184
Abdallah Mahmoud <javabyabdallah@gmail.com>
2025-07-06 15:25:21 +0300

    Fix selection sort.

--- src/main/java/com/riigsoft/sort/SelectionSort.java
+++ src/main/java/com/riigsoft/sort/SelectionSort.java
@@ -1,24 +1,26 @@
 package com.riigsoft.sort;
 
 public class SelectionSort {
-    public void sort(int[] numbers) {
-        for (var i = 0; i < numbers.length; i++) {
-            var minIndex = findMinIndex(numbers, i);
+    public void sort(int[] numbers){
+        for (var i = 0; i < numbers.length; i++){
+//            var mindIndex = i;
+//            for (var j = i; j < numbers.length; j++)
+//                if (numbers[j] < numbers[mindIndex])
+//                    mindIndex =j;
+            int minIndex = findMinIndex(numbers, i);
             swap(numbers, minIndex, i);
         }
     }
-
-    private void swap(int[] numbers, int index1, int index2) {
+    private  void swap(int [] numbers, int index1, int index2){
         var temp = numbers[index1];
         numbers[index1] = numbers[index2];
         numbers[index2] = temp;
     }
+    private int findMinIndex(int[] numbers, int minIndex) {
+        for (var i = minIndex; i < numbers.length; i++)
+            if (numbers[i] < numbers[minIndex])
+                minIndex = i;
 
-    private int findMinIndex(int[] numbers, int mindIndex) {
-        for (var j = mindIndex; j < numbers.length; j++)
-            if (numbers[j] < numbers[mindIndex])
-                mindIndex = j;
-
-        return mindIndex;
+        return minIndex;
     }
 }
